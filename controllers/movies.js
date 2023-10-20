a4f6c02d5926be04d9da5cf10f32ad324f940d41
kfac <kfacison1998@gmail.com>
2023-10-20 13:56:37 -0500

    spacing

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,47 +1,47 @@
 const Movie = require('../models/movie');
 
 module.exports = {
-  index,
-  show,
-  new: newMovie,
-  create
+    index,
+    show,
+    new: newMovie,
+    create
 };
 
 async function index(req, res) {
-  const movies = await Movie.find({});
-  res.render('movies/index', { title: 'All Movies', movies });
+    const movies = await Movie.find({});
+    res.render('movies/index', { title: 'All Movies', movies });
 }
 
 async function show(req, res) {
-  const movie = await Movie.findById(req.params.id);
-  res.render('movies/show', { title: 'Movie Detail', movie });
+    const movie = await Movie.findById(req.params.id);
+    res.render('movies/show', { title: 'Movie Detail', movie });
 }
 
 function newMovie(req, res) {
   // We'll want to be able to render an  
   // errorMsg if the create action fails
-  res.render('movies/new', { title: 'Add Movie', errorMsg: '' });
+    es.render('movies/new', { title: 'Add Movie', errorMsg: '' });
 }
 
 async function create(req, res) {
   // convert nowShowing's checkbox of nothing or "on" to boolean
-  req.body.nowShowing = !!req.body.nowShowing;
+    req.body.nowShowing = !!req.body.nowShowing;
   // remove any whitespace at start and end of cast
-  req.body.cast = req.body.cast.trim();
+    req.body.cast = req.body.cast.trim();
   // split cast into an array if it's not an empty string - using a regular expression as a separator
-  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+    if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
   // Remove empty properties so that defaults will be applied
-  for (let key in req.body) {
+    for (let key in req.body) {
     if (req.body[key] === '') delete req.body[key];
-  }
-  try {
-    await Movie.create(req.body);
-    // Always redirect after CUDing data
-    // We'll refactor to redirect to the movies index after we implement it
-    res.redirect('/movies');  // Update this line
-  } catch (err) {
-    // Typically some sort of validation error
-    console.log(err);
-    res.render('movies/new', { errorMsg: err.message });
-  }
+    }
+    try {
+        await Movie.create(req.body);
+        // Always redirect after CUDing data
+        // We'll refactor to redirect to the movies index after we implement it
+        res.redirect('/movies');  // Update this line
+    } catch (err) {
+        // Typically some sort of validation error
+        console.log(err);
+        res.render('movies/new', { errorMsg: err.message });
+    }
 }
