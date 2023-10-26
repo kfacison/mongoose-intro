ed6c75f15533a03414dfb367afef5bd85ce458ca
kfac <kfacison1998@gmail.com>
2023-10-26 12:59:22 -0500

    added oAuth

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 
 require('dotenv').config();
 // connect to the database with AFTER the config vars are processed
 require('./config/database');
+require('./config/passport');
 
 const indexRouter = require('./routes/index');
 const moviesRouter = require('./routes/movies');
@@ -25,6 +28,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add this middleware BELOW passport middleware
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/movies', moviesRouter);
