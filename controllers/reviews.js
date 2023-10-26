d1af485f7779c00710d90698339f73b6743f1f91
kfac <kfacison1998@gmail.com>
2023-10-26 14:29:53 -0500

    lunch break

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,7 +1,8 @@
 const Movie = require('../models/movie');
 
 module.exports = {
-  create
+  create,
+  delete: deleteReview
 };
 
 async function create(req, res) {
@@ -20,4 +21,17 @@ async function create(req, res) {
     console.log(err);
   }
   res.redirect(`/movies/${movie._id}`);
+}
+
+async function deleteReview(req, res){
+
+  // find the reviw in the array and then splice
+  //movie.reviews.splice(req.body,1);
+  try {
+    // Save any changes made to the movie doc
+    await movie.save();
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect(`/movies/${movie._id}`)
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,5 +4,6 @@ const reviewsCtrl = require('../controllers/reviews');
 
 // POST /movies/:id/reviews (create review for a movie)
 router.post('/movies/:id/reviews', reviewsCtrl.create);
+router.delete('/', reviewsCtrl.delete);
 
 module.exports = router;
